1b95db2c050f6c7df383220692e771abbcf07305
rndpatel@gmail.com <rndpatel@gmail.com>
2019-11-04 13:02:55 -0800

    added useContext to products

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import ProductContext from "./context/ProductContext";
+
 function App() {
+
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
@@ -17,26 +21,23 @@ function App() {
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value ={{products, addItem}}>
+			<div className="App">
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				{/* Routes */}
+				<Route
+					exact
+					path="/"
+					component = {Products}
+				/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
+			</div>
+		</ProductContext.Provider>
 	);
 }
 
